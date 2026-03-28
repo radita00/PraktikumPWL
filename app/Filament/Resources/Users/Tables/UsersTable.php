15554f701c2022315f778d9c542bfb5d2d6a223b
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-03-28 08:59:27 +0700

    tampil data tabel

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
 
 class UsersTable
 {
@@ -13,7 +14,8 @@ class UsersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
             ])
             ->filters([
                 //
