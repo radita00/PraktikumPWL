2c224a30da1fb7530804473b5551699d6c1de01c
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-03-30 23:09:37 +0700

    JOBSHEET 6 (1)

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TagsInput;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make("title")
+                    ->minLength(5),
+                TextInput::make("slug")
+                    ->unique(ignoreRecord: true),
+                Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->preload()
+                    ->searchable(),
+                ColorPicker::make("color"),
+                MarkdownEditor::make("content"),
+                // RichEditor::make("content"),
+                FileUpload::make("image")
+                ->disk("public")
+                ->directory("posts"),
+                TagsInput::make("tags"),
+                Checkbox::make("published"),
+                DateTimePicker::make("published_at"),
+            ])->columns(3);
+    }
+}
