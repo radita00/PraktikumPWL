2bcc2fcc8ba05f10f1e27dd3f76b3ae3090f7264
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-03-31 08:30:34 +0700

    custom layout form

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -12,30 +12,69 @@ use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
 
 class PostForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(3)
             ->components([
-                TextInput::make("title")
-                    ->minLength(5),
-                TextInput::make("slug")
-                    ->unique(ignoreRecord: true),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make("color"),
-                MarkdownEditor::make("content"),
-                // RichEditor::make("content"),
-                FileUpload::make("image")
-                ->disk("public")
-                ->directory("posts"),
-                TagsInput::make("tags"),
-                Checkbox::make("published"),
-                DateTimePicker::make("published_at"),
-            ])->columns(3);
+
+                // KIRI (2/3)
+                Section::make("Post Details")
+                    ->description("Fill in the details of the post")
+                    ->icon('heroicon-o-document-text')
+                    ->schema([
+
+                        // 2 KOLOM UTAMA
+                        Group::make([
+                            TextInput::make('title')
+                                ->required(),
+
+                            TextInput::make('slug'),
+
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->searchable(),
+
+                            ColorPicker::make('color'),
+                        ])->columns(2),
+
+                        // FULL WIDTH
+                        MarkdownEditor::make('body')
+                            ->columnSpanFull(),
+
+                        MarkdownEditor::make('content')
+                            ->columnSpanFull(),
+                    ])
+                    ->columnSpan(2),
+
+                // KANAN (1/3)
+                Group::make([
+
+                    Section::make('Image Upload')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('post'),
+                        ]),
+
+                    Section::make('Meta')
+                        ->icon('heroicon-o-cog-6-tooth')
+                        ->schema([
+                            TagsInput::make('tags'),
+                            Checkbox::make('published'),
+                            DateTimePicker::make('published_at'),
+                        ]),
+
+                ])
+                ->columnSpan(1),
+
+            ]);
     }
 }
